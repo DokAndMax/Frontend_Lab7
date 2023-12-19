f9122a39912f534a9c65c6c3f82fb46a58d651c0
Максим Кривенок <maxim.kryvenok@gmail.com>
2023-12-19 23:13:56 +0200

    Clean and gh-pages integration

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Image from "./components/Image";
 import React from "react";
-import GoodsCard from "./components/GoodsCard";
 import GoodsGallery from "./components/GoodsGallery";
 
 function App() {
-    const products = [
-        { title: 'Cabbage', isFruit: false, id: 1 },
-        { title: 'Garlic', isFruit: false, id: 2 },
-        { title: 'Apple', isFruit: true, id: 3 },
-    ];
-
   return (
     <div className="App">
       <Header name="Кривенок Максим Геннадійович"/>
--- src/components/GoodsGallery.js
+++ src/components/GoodsGallery.js
@@ -1,43 +1,43 @@
 // Компонент GoodsGallery, який відображає список товарів у вигляді галереї
 import React from 'react';
 import GoodsCard from './GoodsCard';
-import styles from './GoodsGallery.module.css'
+import styles from './GoodsGallery.module.css';
 
 function GoodsGallery() {
     const products = [
         {
             id: 1,
-            image: 'goodsImages/blueberry.png',
+            image: './goodsImages/blueberry.png',
             name: 'Product 1',
             price: '10',
         },
         {
             id: 2,
-            image: 'goodsImages/cherry.png',
+            image: './goodsImages/cherry.png',
             name: 'Product 2',
             price: '20',
         },
         {
             id: 3,
-            image: 'goodsImages/peach.png',
+            image: './goodsImages/peach.png',
             name: 'Product 3',
             price: '30',
         },
         {
             id: 4,
-            image: 'goodsImages/pineapple.png',
+            image: './goodsImages/pineapple.png',
             name: 'Product 4',
             price: '40',
         },
         {
             id: 5,
-            image: 'goodsImages/raspberry.png',
+            image: './goodsImages/raspberry.png',
             name: 'Product 5',
             price: '50',
         },
         {
             id: 6,
-            image: 'goodsImages/strawberry.png',
+            image: './goodsImages/strawberry.png',
             name: 'Product 6',
             price: '60',
         },
